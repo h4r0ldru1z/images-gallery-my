e7d8dc318bada162c650bc304da8bc025133bc85
Harold Ruiz Jovel <haroldand.ruiz@gmail.com>
2022-12-11 19:21:09 -0500

    final format adjustment

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -10,12 +10,12 @@ const App = () => {
   const [word, setWord] = useState('');
 
   const handleSearchSubmit = (e) => {
-    e.preventDefault(); /*previene que se cargue la página*/
+    e.preventDefault();
+    /*previene que se cargue la página*/
     /*console.log(e.target[0].value);así no podemos sincronizar el tema, debemos usar una forma que sea un conmponente controlado para poder hacer el seguimiento en react*/
     /*vamos a almacenar en el state todo lo que el usuario ingrese, haciendo el formulario un componente controlado, y tomamos del valor del state y no del formulario*/
-    console.log(
-      word
-    ); /*Va después de la función App, porque antes no teníamos la variable word*/
+    console.log(word);
+    /*Va después de la función App, porque antes no teníamos la variable word*/
     fetch(
       `https://api.unsplash.com/photos/random/?query=${word}&client_id=${UNSPLASH_KEY}`
     )
