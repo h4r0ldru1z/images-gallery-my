718dca925a7b4dd3e9b61036f2168e7c91e2fdec
Harold Ruiz Jovel <haroldand.ruiz@gmail.com>
2022-12-11 19:19:06 -0500

    fixed code with linter

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,37 +3,38 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import Header from './components/Header';
 import Search from './components/Search';
 
-const UNSPLASH_KEY=process.env.REACT_APP_UNSLPASH_KEY;
+const UNSPLASH_KEY = process.env.REACT_APP_UNSLPASH_KEY;
 
 /*will have everythng to fetch, and search*/
-const App=()=> {
-  const [word, setWord]=useState('');
-  
-  const handleSearchSubmit = (e) =>{
-    e.preventDefault() /*previene que se cargue la página*/
+const App = () => {
+  const [word, setWord] = useState('');
+
+  const handleSearchSubmit = (e) => {
+    e.preventDefault(); /*previene que se cargue la página*/
     /*console.log(e.target[0].value);así no podemos sincronizar el tema, debemos usar una forma que sea un conmponente controlado para poder hacer el seguimiento en react*/
     /*vamos a almacenar en el state todo lo que el usuario ingrese, haciendo el formulario un componente controlado, y tomamos del valor del state y no del formulario*/
-    console.log(word); /*Va después de la función App, porque antes no teníamos la variable word*/
-    fetch(`https://api.unsplash.com/photos/random/?query=${word}&client_id=${UNSPLASH_KEY}`)
-      .then((res)=>res.json())
-      .then((data) =>{
+    console.log(
+      word
+    ); /*Va después de la función App, porque antes no teníamos la variable word*/
+    fetch(
+      `https://api.unsplash.com/photos/random/?query=${word}&client_id=${UNSPLASH_KEY}`
+    )
+      .then((res) => res.json())
+      .then((data) => {
         console.log(data);
       })
-      .catch((err)=>{
+      .catch((err) => {
         console.log(err);
-      })
+      });
     setWord('');
-
-  }
-
-  
+  };
 
   return (
     <div className="App">
-      <Header title="Images Gallery"/>
-      <Search word={word} setWord={setWord} handleSubmit={handleSearchSubmit}/>
+      <Header title="Images Gallery" />
+      <Search word={word} setWord={setWord} handleSubmit={handleSearchSubmit} />
     </div>
   );
-}
+};
 
 export default App;
--- frontend/src/components/Search.js
+++ frontend/src/components/Search.js
@@ -4,30 +4,37 @@ import { Container, Row, Col, Form, Button } from 'react-bootstrap';
 /*xs 9 toma 9 divido 12 del ancho de la columna padre, 
 línea 10. así que el formulario toma 9/12 de ese ancho, que sería 2/3*/
 const Search = ({ word, setWord, handleSubmit }) => {
-    return (
-        <Container className="mt-4">
-            <Row className='justify-content-center'>
-                <Col xs={12} md={8} lg={6}>
-                <Form onSubmit={handleSubmit}>
-                    <Form.Row>
-                        <Col xs={9}>
-                            <Form.Control
-                                type="text"
-                                value={word} /*connect value del input con el state de la aplicación*/
-                                onChange={(e) =>setWord(e.target.value)} /*cada vez que cambie, se llamará el evento setWord*, con el valor del evento, es decir las letras o cambios, del input form*/
-                                placeholder="Search for new image... "/>
-                        </Col>
-                        <Col>
-                            <Button variant="primary" type="submit">Search</Button>
-                        </Col>
-                    </Form.Row>
-                </Form>
-                </Col>
-            </Row>
-        </Container>
-    )
+  return (
+    <Container className="mt-4">
+      <Row className="justify-content-center">
+        <Col xs={12} md={8} lg={6}>
+          <Form onSubmit={handleSubmit}>
+            <Form.Row>
+              <Col xs={9}>
+                <Form.Control
+                  type="text"
+                  value={
+                    word
+                  } /*connect value del input con el state de la aplicación*/
+                  onChange={(e) =>
+                    setWord(e.target.value)
+                  } /*cada vez que cambie, se llamará el evento setWord*, con el valor del evento, es decir las letras o cambios, del input form*/
+                  placeholder="Search for new image... "
+                />
+              </Col>
+              <Col>
+                <Button variant="primary" type="submit">
+                  Search
+                </Button>
+              </Col>
+            </Form.Row>
+          </Form>
+        </Col>
+      </Row>
+    </Container>
+  );
 };
 
 export default Search;
 
-/*creo la función en APP, que es el principal, y la llamo como propiedad, que será handle Submit. Entonces cuando haga click en submit, llamará la propiedad creada, que está ligada a la función en APP*/ 
+/*creo la función en APP, que es el principal, y la llamo como propiedad, que será handle Submit. Entonces cuando haga click en submit, llamará la propiedad creada, que está ligada a la función en APP*/
