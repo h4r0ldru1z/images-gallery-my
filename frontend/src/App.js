f472a3fd7c3cf72134a6ae66c8f044ac83a045ee
Harold Ruiz Jovel <haroldand.ruiz@gmail.com>
2022-12-11 21:04:44 -0500

    display and delete images

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,8 @@ import { useState } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Header from './components/Header';
 import Search from './components/Search';
+import ImageCard from './components/ImageCard';
+import { Container, Row, Col } from 'react-bootstrap';
 
 const UNSPLASH_KEY = process.env.REACT_APP_UNSLPASH_KEY;
 
@@ -10,21 +12,18 @@ const App = () => {
   const [word, setWord] = useState('');
   const [images, setImages] = useState([]);
 
-  console.log(images);
-
   const handleSearchSubmit = (e) => {
     e.preventDefault();
     /*previene que se cargue la página*/
     /*console.log(e.target[0].value);así no podemos sincronizar el tema, debemos usar una forma que sea un conmponente controlado para poder hacer el seguimiento en react*/
     /*vamos a almacenar en el state todo lo que el usuario ingrese, haciendo el formulario un componente controlado, y tomamos del valor del state y no del formulario*/
-    console.log(word);
     /*Va después de la función App, porque antes no teníamos la variable word*/
     fetch(
       `https://api.unsplash.com/photos/random/?query=${word}&client_id=${UNSPLASH_KEY}`
     )
       .then((res) => res.json())
       .then((data) => {
-        setImages([data, ...images]);
+        setImages([{ ...data, title: word }, ...images]);
       })
       .catch((err) => {
         console.log(err);
@@ -32,12 +31,32 @@ const App = () => {
     setWord('');
   };
 
+  /*if image.id = id, the image is filtered out of new array*/
+  const handleDeleteImage = (id) => {
+    setImages(images.filter((image) => image.id !== id));
+  };
+
   return (
     <div className="App">
       <Header title="Images Gallery" />
       <Search word={word} setWord={setWord} handleSubmit={handleSearchSubmit} />
+      <Container className="mt-4">
+        <Row xs={1} md={2} lg={3}>
+          {images.map((image, i) => (
+            <Col key={i} className="pb-3">
+              <ImageCard image={image} deleteImage={handleDeleteImage} />
+            </Col>
+          ))}
+        </Row>
+      </Container>
     </div>
   );
 };
-
+/* voy a usar las propiedades de la card y del array imágenes, pero cuando está sin llenar,
+      va a dar error, por eso se llama el condicional && que solo lo hace si es
+      true, mientras que si está sin valores es false y el !! lo convierte en
+      booleano para que no salga 0 en la página */
+/* map es un método de ayuda para el array, y va por todos los elementos, y
+genera uno nuevo, para el ejemplo, es un array nuevo del componente ImageCard. La imagen del componente, 
+es la imagen del array, y el uso del índice es para el key único. */
 export default App;
--- frontend/src/components/Imagecard.js
+++ frontend/src/components/Imagecard.js
@@ -1,7 +1,19 @@
 import React from 'react';
+import { Card, Button } from 'react-bootstrap';
 
-const imagecard = () => {
-  return <div>Welcome</div>;
+const ImageCard = ({ image, deleteImage }) => {
+  return (
+    <Card style={{ width: '18rem' }}>
+      <Card.Img variant="top" src={image.urls.small} />
+      <Card.Body>
+        <Card.Title>{image.title.toUpperCase()}</Card.Title>
+        <Card.Text>{image.description || image.alt_description}</Card.Text>
+        <Button variant="primary" onClick={() => deleteImage(image.id)}>
+          Delete
+        </Button>
+      </Card.Body>
+    </Card>
+  );
 };
 
-export default imagecard;
+export default ImageCard;
