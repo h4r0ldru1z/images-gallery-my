ee4bb9bb9a23c4be263c65740985208786929a73
Harold Ruiz Jovel <haroldand.ruiz@gmail.com>
2022-12-11 19:54:08 -0500

    images in the state

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -8,6 +8,9 @@ const UNSPLASH_KEY = process.env.REACT_APP_UNSLPASH_KEY;
 /*will have everythng to fetch, and search*/
 const App = () => {
   const [word, setWord] = useState('');
+  const [images, setImages] = useState([]);
+
+  console.log(images);
 
   const handleSearchSubmit = (e) => {
     e.preventDefault();
@@ -21,7 +24,7 @@ const App = () => {
     )
       .then((res) => res.json())
       .then((data) => {
-        console.log(data);
+        setImages([data, ...images]);
       })
       .catch((err) => {
         console.log(err);
---
+++ frontend/src/components/Imagecard.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const imagecard = () => {
+  return <div>Welcome</div>;
+};
+
+export default imagecard;
